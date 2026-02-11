01f0b9df6d51245d481f89ef36141dea7359ea52
nibleroAGR <niblero@gmail.com>
2026-02-12 00:52:49 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,52 @@
+const CACHE_NAME = 'fiesta-party-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json',
+  '/fiesta_logo.png'
+];
+
+// Instalar el Service Worker y cachear activos estáticos
 self.addEventListener('install', event => {
-  console.log('Service Worker instalado');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Abriendo caché y guardando archivos');
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
 });
 
+// Activar el Service Worker y limpiar cachés antiguos
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('Eliminando caché antiguo:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Estrategia Stale-while-revalidate: sirve desde el cache y actualiza en segundo plano
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        const fetchPromise = fetch(event.request).then(networkResponse => {
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, networkResponse.clone());
+          });
+          return networkResponse;
+        });
+        return response || fetchPromise;
+      })
+  );
 });
